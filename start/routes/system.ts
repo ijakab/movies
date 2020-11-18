1bee65b81c0c6a79a08dc50c1820e270d0691513
Ivan Jakab <ivan.jakab@gauss.hr>
2020-11-18 22:02:20 +0100

    fix: eslint rules

--- start/routes/system.ts
+++ start/routes/system.ts
@@ -1,9 +1,9 @@
 import Route from '@ioc:Adonis/Core/Route'
 import HealthCheck from '@ioc:Adonis/Core/HealthCheck'
+import { HttpContext } from '@adonisjs/http-server/build/src/HttpContext'
 
-Route.get('health', async ({ response }) => {
+Route.get(`health`, async ({ response }: HttpContext) => {
   const report = await HealthCheck.getReport()
-
   return report.healthy
     ? response.ok(report)
     : response.badRequest(report)
