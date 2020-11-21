18a1293ef6456ed0b89fddc7b1bee2ec0cc1161c
Ivan Jakab <ivan.jakab@gauss.hr>
2020-11-21 20:08:50 +0100

    docs: fixed typo

--- start/routes/bindModuleController.ts
+++ start/routes/bindModuleController.ts
@@ -8,7 +8,7 @@ export function bindModuleController (
 ): RouteContract {
   // This is a temporary fix. Adonis seems to append App/Controller/Http path before anything we send, as it is standard adonis dir structure
   // However, having modular structure is really useful in bigger projects. So this ../.. negates Controller/Http
-  // Also remove Http part as I find it useless - WS controllers will probably be on separate project because they need to be deployed and scaled differently
+  // Also removed Http part as I find it useless - WS controllers will probably be on separate project because they need to be deployed and scaled differently
   // Protocols other than http will usually not be supported on serverless and are probably anti pattern
   return Route.get(route, `../../Modules/${module}/Controllers/${controller}.controller.${method}`);
 }
