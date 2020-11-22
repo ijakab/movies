287630fb4313b945c60bf7cb1fa8908089f63d27
Ivan Jakab <ivan.jakab@gauss.hr>
2020-11-22 13:42:29 +0100

    fix: ts ignore

--- app/Common/Controllers/base.controller.ts
+++ app/Common/Controllers/base.controller.ts
@@ -3,6 +3,9 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import { BaseModel } from '@adonisjs/lucid/build/src/Orm/BaseModel';
 import { GlobalResponseDto } from 'App/Common/Dto/global-response.dto';
 
+// We need an ignore line below, as we do not know here what models will services use.
+// Typescript does not recognize derived classes as assignable to base classes when typeof is used
+// @ts-ignore
 export abstract class BaseController <Service extends BaseService> {
   protected service: Service;
 
